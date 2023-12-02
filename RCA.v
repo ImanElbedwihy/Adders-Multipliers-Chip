43a11526ebbad734b760eb47ba8a3faca84cb491
Sara Gamal <sarah.gerges01@eng-st.cu.edu.eg>
2023-12-02 20:49:30 +0200

    adding generate bfore the for

--- RCA.v
+++ RCA.v
@@ -5,9 +5,13 @@ module RCA
 wire [32:0] w_C;
 genvar i;
 assign w_C[0]=Cin;
+generate 
 for (i=0;i<32;i=i+1)
+ begin :loop
     full_adder full_adder_bit_1
         ( .i_bit1(a[i]),.i_bit2(b[i]), .i_carry(w_C[i]), .o_sum(sum[i]), .o_carry(w_C[i+1])); 
+		  end
+		    endgenerate
 assign Cout=w_C[32];
 assign of= ~(a[31]^b[31])&(a[31]^sum[31]);
 
